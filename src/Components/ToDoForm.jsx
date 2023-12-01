4c9a8655a903a5d67848d55083e3adc482342944
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-01 02:39:16 -0700

    lab 6

--- src/Components/ToDoForm.jsx
+++ src/Components/ToDoForm.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 function ToDoForm({ addTask }) {
     const [taskText, setTaskText] = useState('');
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+        try {
+            const response = await fetch('./data/tasks.json');
+            const data = await response.json();
+            setTasks(data;)
+            } catch (error) {
+                console.error('Error fetching tasks:', error);
+            }
+        };
+
+        fetchTasks();
+    }, []);
 
     const handleAddTask = () => {
         if (task.trim() !== '') {
@@ -10,6 +24,15 @@ function ToDoForm({ addTask }) {
          setTask('');
         }
     };
+
+    const handleGenerateRandomTask = () => {
+        if (tasks.length > 0) {
+          const randomIndex = Math.floor(Math.random() * tasks.length);
+          const randomTask = tasks[randomIndex].task;
+          setSelectedTask(randomTask);
+        }
+      };
+
   return (
     <View style={styles.form}>
         <TextInput
@@ -19,6 +42,7 @@ function ToDoForm({ addTask }) {
             value={taskText}
         />
        <Button title="Add Task" onPress={() => addTask(taskText)} />
+       <Button title="Generate Random Task" onPress={handleGenerateRandomTask} />
 
     </View>
   );
